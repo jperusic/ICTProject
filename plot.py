736e25fbc2c558b233656cff9b3b55f1d319445a
winticles <44012382+winticles@users.noreply.github.com>
2020-09-16 15:26:21 +0930

    Various Code Cleanup
    Cleaned up some code and comments

--- plot.py
+++ plot.py
@@ -1,4 +1,4 @@
-## import libraries for plotting and data reading
+## Import the libraries for plotting and data reading
 import matplotlib.pyplot as plt
 import csv
 
@@ -15,62 +15,62 @@ inFile.close
 
 def plot(fp):
 
-    ## declaration of variables used in the process
-    w = [] ## holds wavelength data to plot
-    s1 = [] ## holds spectra data to plot
-    s2 = [] ## holds spectra data to plot
-    s3 = [] ## holds spectra data to plot
+    ## Declaration of the variables used in the process
+    w = [] ## Holds the wavelength data to plot
+    s1 = [] ## Holds the spectra data to plot
+    s2 = [] ## Holds the spectra data to plot
+    s3 = [] ## Holds the spectra data to plot
 
-    valuesToPlot = 0 ## defaults plotting requirements to 0
+    valuesToPlot = 0 ## Defaults the plotting requirements to 0
 
-    ## read data and split at "," save individual values to plot
-    ## for each entry in the plot values
+    ## Read the data and split at "," save individual values to plot
+    ## For each entry in the plot values
     plot = csv.reader(data, delimiter=",")  
     for row in plot:
-        ## if the error message was thrown (only 1 message) - graph will show with no content
+        ## If the error message was thrown (only 1 message) - the graph will show with no content
         if len(row) == 1:
             valuesToPlot = 1
-        ## if one spectra value  
-        ## append as plottable values to lists  
+        ## If one spectra value exists 
+        ## Append as plottable values to lists  
         if len(row) > 1:
             w.append(float(row[0]))
             s1.append(float(row[1]))
             valuesToPlot = 2
-            ## if two spectra values
+            ## If two spectra values exist
             if len(row) > 2:
                 s2.append(float(row[2]))
                 valuesToPlot = 3
-            ## if three spectra values
+            ## If three spectra values exist
             if len(row) > 3:    
                 s3.append(float(row[3]))
                 valuesToPlot = 4
 
-    ## if there is plottable data 
+    ## If there is plottable data 
     if valuesToPlot > 0:
-        ## if data present, but processing error 
+        ## If data is present, but processing error 
         if valuesToPlot==1:
-            plt.title(fileName[:8] + " data is corrupt")
-        ## if one spectra value
-        ## plot the wavelength and spectra values with labels
+            plt.title(fileName[:8] + "Data is corrupt.")
+        ## If one spectra value exists
+        ## Plot the wavelength and spectra values with their labels
         if valuesToPlot==2:
             plt.plot(w,s1, label="Spectra")
             plt.title('Spectra Reading for ' + fileName[:8])
-        ## if two spectra value
+        ## If two spectra value exist
         if valuesToPlot==3:
             plt.plot(w,s1, label="Spectra 1")
             plt.plot(s2, label="Spectra 2")
             plt.title('Spectra Reading for ' + fileName[:8])
-        ## if three spectra value
+        ## If three spectra value exist
         if valuesToPlot==4:
             plt.plot(w,s1, label="Spectra 1")
             plt.plot(s2, label="Spectra 2")
             plt.plot(s3, label="Spectra 3")
             plt.title('Spectra Reading for ' + fileName[:8])
 
-        ## title the x and y axis, create a legend and display the graph
-        plt.xlabel('Wavelength')
-        plt.ylabel('Spectra')
-        plt.legend()
-        plt.show()
+        plt.xlabel('Wavelength') ## Title for the X axis   
+        plt.ylabel('Spectra') ## Title for the Y axis
+        plt.legend() ## Create a legend
+        plt.show() ## Display the graph to the user
 
+## Calls the function 'plot', passing in the inFile variable as the path name
 plot(inFile)
