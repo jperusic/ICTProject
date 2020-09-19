c5841f8898badc3c9aee30475f13e5f7209538b6
jperusic <69899918+jperusic@users.noreply.github.com>
2020-09-20 05:41:22 +0930

    CORRECTED: Resized/Dynamic Window & Pane Support
    **Wrong file was uploaded (older version of gui.py) this version is up to date**
    Min window size to avoid button collapse.
    Equal divide of the file list pane and preview panes - support for dynamic resizing
    Footer configured to bottom of the window with appropriate padding to not clash w/ other panes.
    Scrollbars for panes with file/preview lists created and mapped.
    GUI finalised - Button colour works on Windows OS, not w/ Mac OS

--- gui.py
+++ gui.py
@@ -40,25 +40,19 @@ tabControl = ttk.Notebook(window)
 ## create layout for Raw Data tab - 2 panes flows vertically
 ## top row to hold buttons - 2 buttons flows horizontally
 ## bottom row to hold file list and preview of selected file - 2 panes flows horizonatally 
-## footer to hold development details
 raw = ttk.PanedWindow(tabControl, orient=VERTICAL)
 rawTop = ttk.PanedWindow(tabControl, orient=HORIZONTAL)
-rawBottom = ttk.PanedWindow(tabControl, orient=HORIZONTAL)
-rawFooter = ttk.PanedWindow(tabControl, orient=HORIZONTAL, height=10)
+rawBottom = ttk.PanedWindow(tabControl, orient=HORIZONTAL, height=500)
 
 ## create layout for Ready Data tab - 2 panes flows vertically
 ## top row to hold buttons - 2 buttons flows horizontally
 ## bottom row to hold file list and preview of selected file - 2 panes flows horizonatally 
-## footer to hold development details
 ready = ttk.PanedWindow(tabControl, orient=VERTICAL)
 readyTop = ttk.PanedWindow(tabControl, orient=HORIZONTAL)
-readyBottom = ttk.PanedWindow(tabControl, orient=HORIZONTAL)
-readyFooter = ttk.PanedWindow(tabControl, orient=HORIZONTAL)
+readyBottom = ttk.PanedWindow(tabControl, orient=HORIZONTAL, height=500)
 
 ## create layout for Plot tab - 1 Pane flows vertically
-## footer to hold development details
 plot = ttk.PanedWindow(tabControl, orient=VERTICAL)
-plotFooter = ttk.PanedWindow(tabControl, orient=HORIZONTAL)
 
 ## create 3 tabs with titles for each stage of the data flow: Raw, Ready, Plot
 ## display tabs on window
@@ -67,6 +61,13 @@ tabControl.add(ready, text="Ready")
 tabControl.add(plot, text="Plot")
 tabControl.pack(expand=1, fill="both")
 
+## create buttons to import raw data files and normalise selected ready data files
+## display buttons on raw tab (top row)
+importRaw = Button(rawTop, text="Import Raw Data", width=20, height=2, command=importFunction, bg="blue", fg="white")
+normaliseData = Button(rawTop, text="Normalise Data", width=20, height=2, command=normaliseFunction, bg="green", fg="white")
+importRaw.pack(side=LEFT, padx=(20,10), pady=(10,0))
+normaliseData.pack(side=LEFT, padx=(10,10), pady=(10,0))
+
 ## label and entry box prompts user to enter waveshift offset - this offset will be used in normalisation process
 ## pack entry box to RH side with appropriate padding
 inputLabel = Label(rawTop, text="Enter Waveshift Offset: ")
@@ -74,37 +75,27 @@ rawInput = Entry(rawTop, width=20)
 rawInput.pack(side=RIGHT, padx=(10,20), pady=(10,0))
 inputLabel.pack(side=RIGHT, padx=(0,5), pady=(10,0))
 
-## raw labels to hold details of developers and their contact details for support/enquiries
-## pack labels to footer with appropriate padding
-rawDev = Label(rawFooter, height=1, text="Developed by J.Perusic & J.Winter (2020)")
-rawContact = Label(rawFooter, height=1, text="Contact the Developers at: example@example.com")
-rawContact.pack(side=BOTTOM, pady=(5,0))
-rawDev.pack(side=BOTTOM, pady=(0,0))
+## create pane to hold the file list and a pane to display a preview of a file when selected
+rawFiles = Listbox(rawBottom, selectmode=MULTIPLE)
+rawPreview = Listbox(rawBottom)
 
-## ready labels to hold details of developers and their contact details for support/enquiries
-## pack labels to footer with appropriate padding
-readyDev = Label(readyFooter, height=1, text="Developed by J.Perusic & J.Winter (2020)")
-readyContact = Label(readyFooter, height=1, text="Contact the Developers at: example@example.com")
-readyContact.pack(side=BOTTOM, pady=(5,0))
-readyDev.pack(side=BOTTOM, pady=(0,0))
+## scrollbar for file list on raw tab
+rawScroll = Scrollbar(rawFiles)
+rawScroll.pack(side=RIGHT, fill=Y)
+rawFiles.config(yscrollcommand=rawScroll.set)
+rawScroll.config(command=rawFiles.yview)
 
-## plot labels to hold details of developers and their contact details for support/enquiries
-## pack labels to footer with appropriate padding
-plotDev = Label(plotFooter, height=1, text="Developed by J.Perusic & J.Winter (2020)")
-plotContact = Label(plotFooter, height=1, text="Contact the Developers at: example@example.com")
-plotContact.pack(side=BOTTOM, pady=(5,0))
-plotDev.pack(side=BOTTOM, pady=(0,0))
+## scrollbar for preview list on raw tab
+rawScroll2 = Scrollbar(rawPreview)
+rawScroll2.pack(side=RIGHT, fill=Y)
+rawPreview.config(yscrollcommand=rawScroll2.set)
+rawScroll2.config(command=rawPreview.yview)
 
-## create button to import raw data files
-## create button to normalise selected raw data files
-## display buttons on raw tab (top row)
-importRaw = Button(rawTop, text="Import Raw Data", width=20, height=2, command=importFunction, bg="blue", fg="white")
-normaliseData = Button(rawTop, text="Normalise Data", width=20, height=2, command=normaliseFunction, bg="green", fg="white")
-importRaw.pack(side=LEFT, padx=(20,10), pady=(10,0))
-normaliseData.pack(side=LEFT, padx=(10,10), pady=(10,0))
+## demo data to populate file lists
+for rawList in ["file1", "file2", "file3", "file4", "etc"]:
+    rawFiles.insert(END, rawList)
 
-## create button to import ready data files
-## create button to plot selected ready data files
+## create buttons to import ready data files and plot selected ready data files
 ## display buttons on ready tab (top row)
 importReady = Button(readyTop, text="Import Ready Data", width=20, height=2, command=importReadyDataFunction, bg="blue", fg="white")
 plotData = Button(readyTop, text="Plot Data", width=20, height=2, command=plotFunction, bg="green", fg="white")
@@ -112,47 +103,51 @@ importReady.pack(side=LEFT, padx=(20,10), pady=(10,0))
 plotData.pack(side=LEFT, padx=(10,10), pady=(10,0))
 
 ## create pane to hold the file list and a pane to display a preview of a file when selected
-rawFiles = Listbox(rawBottom, width=60, height=17)
-rawPreview = Listbox(rawBottom, width=60, height=17)
+readyFiles = Listbox(readyBottom, selectmode=MULTIPLE, yscrollcommand=TRUE)
+readyPreview = Listbox(readyBottom)
 
-## create pane to hold the file list and a pane to display a preview of a file when selected
-readyFiles = Listbox(readyBottom, width=55, height=32)
-readyPreview = Listbox(readyBottom, width=55, height=32)
+## scrollbar for file list on ready tab
+readyScroll = Scrollbar(readyFiles)
+readyScroll.pack(side=RIGHT, fill=Y)
+readyFiles.config(yscrollcommand=readyScroll.set)
+readyScroll.config(command=readyFiles.yview)
+
+## scrollbar for preview list on ready tab
+readyScroll2 = Scrollbar(readyPreview)
+readyScroll2.pack(side=RIGHT, fill=Y)
+readyPreview.config(yscrollcommand=readyScroll2.set)
+readyScroll2.config(command=readyPreview.yview)
+
+## demo data to populate file lists
+for readyList in ["file1", "file2", "file3", "file4", "etc"]:
+    readyFiles.insert(END, readyList)
 
 ## create single pane to display graph generated by the plot button function
 plotPreview = Frame(plot, height=600)
 
-## demo data to populate file lists
-for rawList in ["file1", "file2", "file3", "file4", "etc"]:
-    rawFiles.insert(END, rawList)
-
-for readyList in ["file1", "file2", "file3", "file4", "etc"]:
-    readyFiles.insert(END, readyList)
-
-## add the created panes which hold and preview raw data files to the raw data tab (bottom row)
-rawBottom.add(rawFiles)
-rawBottom.add(rawPreview)
-
-
-
-## add the created panes which hold and preview ready data files to the ready data tab (bottom row)
-readyBottom.add(readyFiles)
-readyBottom.add(readyPreview)
+## raw labels to hold details of developers and their contact details for support/enquiries
+## pack labels to footer with appropriate padding
+rawDev = Label(window, height=1, text="Developed by J.Perusic & J.Winter (2020)")
+rawContact = Label(window, height=1, text="Contact the Developers at: example@example.com")
+rawContact.pack(side=BOTTOM, pady=(5,5))
+rawDev.pack(side=BOTTOM, pady=(0,0))
 
 ## add the created panes which hold and the top and bottom rows of the raw tab
+## pack file list and preview panes to the raw tab
 raw.add(rawTop)
 raw.add(rawBottom)
-raw.add(rawFooter)
+rawFiles.pack(side=LEFT, expand=2, fill="both")
+rawPreview.pack(side=RIGHT, expand=2, fill="both")
 
+## pack file list and preview panes to the ready tab
 ## add the created panes which hold and the top and bottom rows of the ready tab
 ready.add(readyTop)
 ready.add(readyBottom)
-ready.add(readyFooter)
+readyFiles.pack(side=LEFT, expand=2, fill="both")
+readyPreview.pack(side=RIGHT, expand=2, fill="both")
 
 ## add the created panes which holds the generated graph to the plot tab
 plot.add(plotPreview)
-plot.add(plotFooter)
+
 ## display the window on run
 window.mainloop()
-
-##importRaw
