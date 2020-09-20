2281578c6a3e9b869398fbb697cdadcdb5a4a491
jperusic <69899918+jperusic@users.noreply.github.com>
2020-09-20 14:54:55 +0930

    Final Layout
    Buttons for Scatter / Line Plot options
    Tested on Mac OSX and have button colours and minsize / resizing as desired, confirmation req again on Windows end
    General code structure tidy
    Footer always present in the window, doesnt collapse on resize
    Only the panes resize with the window, all elements stay same size

--- gui.py
+++ gui.py
@@ -30,8 +30,8 @@ def plotFunction():
 ## new tkinter window - labelled window
 ## title and size of window set
 window = tk.Tk()
-window.geometry("1080x720")
-window.minsize(720, 400)
+window.geometry("820x420")
+window.minsize(820, 420)
 window.title("Raman Plotter")
 
 ## tabs will use notebook format
@@ -39,20 +39,28 @@ tabControl = ttk.Notebook(window)
 
 ## create layout for Raw Data tab - 2 panes flows vertically
 ## top row to hold buttons - 2 buttons flows horizontally
-## bottom row to hold file list and preview of selected file - 2 panes flows horizonatally 
+## bottom row to hold file list and preview of selected file - 2 panes flows horizonatally
+## create footer to hold dev info 
 raw = ttk.PanedWindow(tabControl, orient=VERTICAL)
 rawTop = ttk.PanedWindow(tabControl, orient=HORIZONTAL)
-rawBottom = ttk.PanedWindow(tabControl, orient=HORIZONTAL, height=500)
+rawBottom = ttk.PanedWindow(tabControl, orient=HORIZONTAL)
+rawFooter = ttk.PanedWindow(tabControl, orient=HORIZONTAL)
 
 ## create layout for Ready Data tab - 2 panes flows vertically
 ## top row to hold buttons - 2 buttons flows horizontally
 ## bottom row to hold file list and preview of selected file - 2 panes flows horizonatally 
+## create footer to hold dev info 
 ready = ttk.PanedWindow(tabControl, orient=VERTICAL)
 readyTop = ttk.PanedWindow(tabControl, orient=HORIZONTAL)
-readyBottom = ttk.PanedWindow(tabControl, orient=HORIZONTAL, height=500)
+readyBottom = ttk.PanedWindow(tabControl, orient=HORIZONTAL)
+readyFooter = ttk.PanedWindow(tabControl, orient=HORIZONTAL)
 
 ## create layout for Plot tab - 1 Pane flows vertically
+## create footer to hold dev info 
 plot = ttk.PanedWindow(tabControl, orient=VERTICAL)
+plotTop = ttk.PanedWindow(tabControl, orient=HORIZONTAL)
+plotBottom = ttk.PanedWindow(tabControl, orient=HORIZONTAL)
+plotFooter = ttk.PanedWindow(tabControl, orient=HORIZONTAL)
 
 ## create 3 tabs with titles for each stage of the data flow: Raw, Ready, Plot
 ## display tabs on window
@@ -63,21 +71,21 @@ tabControl.pack(expand=1, fill="both")
 
 ## create buttons to import raw data files and normalise selected ready data files
 ## display buttons on raw tab (top row)
-importRaw = Button(rawTop, text="Import Raw Data", width=20, height=2, command=importFunction, bg="blue", fg="white")
-normaliseData = Button(rawTop, text="Normalise Data", width=20, height=2, command=normaliseFunction, bg="green", fg="white")
-importRaw.pack(side=LEFT, padx=(20,10), pady=(10,0))
-normaliseData.pack(side=LEFT, padx=(10,10), pady=(10,0))
+importRaw = Button(rawTop, text="Import Raw Data", width=20, height=2, bg="blue", fg="white", highlightbackground='#0000FF', command=importFunction,)
+normaliseData = Button(rawTop, text="Normalise Data", width=20, height=2, bg="green", fg="white", highlightbackground='#008000', command=normaliseFunction, )
+importRaw.pack(side=LEFT, padx=(20,10), pady=(15,10))
+normaliseData.pack(side=LEFT, padx=(10,10), pady=(15,10))
 
 ## label and entry box prompts user to enter waveshift offset - this offset will be used in normalisation process
 ## pack entry box to RH side with appropriate padding
 inputLabel = Label(rawTop, text="Enter Waveshift Offset: ")
-rawInput = Entry(rawTop, width=20)
+rawInput = Entry(rawTop, width=15)
 rawInput.pack(side=RIGHT, padx=(10,20), pady=(10,0))
 inputLabel.pack(side=RIGHT, padx=(0,5), pady=(10,0))
 
 ## create pane to hold the file list and a pane to display a preview of a file when selected
-rawFiles = Listbox(rawBottom, selectmode=MULTIPLE)
-rawPreview = Listbox(rawBottom)
+rawFiles = Listbox(rawBottom, selectmode=MULTIPLE, yscrollcommand=TRUE)
+rawPreview = Listbox(rawBottom, yscrollcommand=TRUE)
 
 ## scrollbar for file list on raw tab
 rawScroll = Scrollbar(rawFiles)
@@ -97,14 +105,14 @@ for rawList in ["file1", "file2", "file3", "file4", "etc"]:
 
 ## create buttons to import ready data files and plot selected ready data files
 ## display buttons on ready tab (top row)
-importReady = Button(readyTop, text="Import Ready Data", width=20, height=2, command=importReadyDataFunction, bg="blue", fg="white")
-plotData = Button(readyTop, text="Plot Data", width=20, height=2, command=plotFunction, bg="green", fg="white")
-importReady.pack(side=LEFT, padx=(20,10), pady=(10,0))
-plotData.pack(side=LEFT, padx=(10,10), pady=(10,0))
+importReady = Button(readyTop, text="Import Ready Data", width=20, height=2, bg="blue", fg="white", highlightbackground='#0000FF', command=importReadyDataFunction)
+plotData = Button(readyTop, text="Plot Data", width=20, height=2, bg="green", fg="white", highlightbackground='#008000', command=plotFunction)
+importReady.pack(side=LEFT, padx=(20,10), pady=(15,10))
+plotData.pack(side=LEFT, padx=(10,10), pady=(15,10))
 
 ## create pane to hold the file list and a pane to display a preview of a file when selected
 readyFiles = Listbox(readyBottom, selectmode=MULTIPLE, yscrollcommand=TRUE)
-readyPreview = Listbox(readyBottom)
+readyPreview = Listbox(readyBottom, yscrollcommand=TRUE)
 
 ## scrollbar for file list on ready tab
 readyScroll = Scrollbar(readyFiles)
@@ -122,32 +130,47 @@ readyScroll2.config(command=readyPreview.yview)
 for readyList in ["file1", "file2", "file3", "file4", "etc"]:
     readyFiles.insert(END, readyList)
 
+## create buttons to plot the current graph as scatter or line 
+## display buttons on plot tab (top row)
+showScatter = Button(plotTop, text="Plot as Scatter", width=20, height=2, bg="blue", fg="white", highlightbackground='#0000FF', command=testScript)
+showLine = Button(plotTop, text="Plot as Line", width=20, height=2, bg="green", fg="white", highlightbackground='#008000', command=testScript)
+showScatter.pack(side=LEFT, padx=(20,10), pady=(15,10))
+showLine.pack(side=LEFT, padx=(10,10), pady=(15,10))
+
 ## create single pane to display graph generated by the plot button function
-plotPreview = Frame(plot, height=600)
+plotPreview = Frame(plotBottom, height=600, bd=2, relief=GROOVE)
 
 ## raw labels to hold details of developers and their contact details for support/enquiries
 ## pack labels to footer with appropriate padding
 rawDev = Label(window, height=1, text="Developed by J.Perusic & J.Winter (2020)")
 rawContact = Label(window, height=1, text="Contact the Developers at: example@example.com")
-rawContact.pack(side=BOTTOM, pady=(5,5))
-rawDev.pack(side=BOTTOM, pady=(0,0))
+rawContact.pack(side=BOTTOM, pady=(0,5))
+rawDev.pack(side=BOTTOM, pady=(5,2))
 
 ## add the created panes which hold and the top and bottom rows of the raw tab
 ## pack file list and preview panes to the raw tab
+## allocate footer space to display dev info
 raw.add(rawTop)
 raw.add(rawBottom)
-rawFiles.pack(side=LEFT, expand=2, fill="both")
-rawPreview.pack(side=RIGHT, expand=2, fill="both")
+rawFiles.pack(side=LEFT, expand=1, fill="both", padx=(0,10))
+rawPreview.pack(side=RIGHT, expand=1, fill="both")
+rawFooter.pack(side=BOTTOM)
 
 ## pack file list and preview panes to the ready tab
 ## add the created panes which hold and the top and bottom rows of the ready tab
+## allocate footer space to display dev info
 ready.add(readyTop)
 ready.add(readyBottom)
-readyFiles.pack(side=LEFT, expand=2, fill="both")
+readyFiles.pack(side=LEFT, expand=2, fill="both", padx=(0,10))
 readyPreview.pack(side=RIGHT, expand=2, fill="both")
+readyFooter.pack(side=BOTTOM)
 
 ## add the created panes which holds the generated graph to the plot tab
-plot.add(plotPreview)
+## allocate footer space to display dev info
+plot.add(plotTop)
+plot.add(plotBottom)
+plotBottom.add(plotPreview)
+plotFooter.pack(side=BOTTOM)
 
 ## display the window on run
 window.mainloop()
