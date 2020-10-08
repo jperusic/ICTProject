5e1be5dd3d5be2f93002372cab064adaa1c1b72c
Jordy <winja006@mymail.unisa.edu.au>
2020-10-08 12:41:23 +1030

    Split functions for plot
    Split the plot_data function into two separate functions; plot_data and plot_data_line. These will make it easier for the GUI to plot as either a line graph or a scatter graph.

--- NEW_plot.py
+++ NEW_plot.py
@@ -16,6 +16,7 @@ class CanvasWidget(_CanvasWidget):
         super().__init__(figure)
         self.ax = figure.add_subplot(111)
 
+## This is the function for plotting a SCATTER PLOT
     def plot_data(self, data: list, title: str = 'Normalised Spectra Plot'):
         """plot data"""
         
@@ -55,10 +56,53 @@ class CanvasWidget(_CanvasWidget):
         self.ax.set_xlabel('Wavelength')
         self.ax.set_xlabel('Spectra')
         self.ax.legend()
+        
+        self.draw()
+## This is the function for plotting a LINE PLOT
+    def plot_data_line(self, data: list, title: str = 'Normalised Spectra Plot'):
+        """plot data"""
+        
+        row0 = data[0]
+        cols = len(row0)
+        self.ax.cla()
+        if cols == 2:
+            wave = [i[0] for i in data]
+            sp = [i[1] for i in data]
 
+            ## display graph as scatter with small points - easier to distinguish on graph
+            self.ax.scatter(wave, sp, label='Spectra', marker=".", linewidths=0)
+
+        elif cols == 3:
+            wave = [i[0] for i in data]
+            sp1 = [i[1] for i in data]
+            sp2 = [i[2] for i in data]
+
+            ## display graph as scatter with small points - easier to distinguish on graph
+            self.ax.scatter(wave, sp1, label='Spectra 1', marker=".", linewidths=0)
+            self.ax.scatter(wave, sp2, label='Spectra 2', marker=".", linewidths=0)
+
+        elif cols == 4:
+            wave = [i[0] for i in data]
+            sp1 = [i[1] for i in data]
+            sp2 = [i[2] for i in data]
+            sp3 = [i[3] for i in data]
+
+            ## display graph as scatter with small points - easier to distinguish on graph
+            self.ax.scatter(wave, sp1, label='Spectra 1', marker=".", linewidths=0)
+            self.ax.scatter(wave, sp2, label='Spectra 2', marker=".", linewidths=0)
+            self.ax.scatter(wave, sp3, label='Spectra 3', marker=".", linewidths=0)
+
+        else:
+            raise ValueError
+        self.ax.set_title(title)
+        self.ax.set_xlabel('Wavelength')
+        self.ax.set_xlabel('Spectra')
+        self.ax.legend()
+        
         self.draw()
 
     def plot_clear(self):
         """clear a plot-"""
         self.ax.cla()
+        ##self.matplolib.show()
         self.draw()
