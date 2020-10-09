761f16c43aca79419d72b8f4ce5cca7c0e9c6344
Yu <xy1527848@gmail.com>
2020-10-09 20:44:11 +0800

    fix and integration

--- data_process.py
+++ data_process.py
@@ -10,172 +10,204 @@ def process(fp, shiftInput: (int, float) = 1) -> list:
     :return: list of normalised data
     """
 
-    results = []
-    ## Open  file target
-    ## specify file name to be used when writing out new data
-    ## imported file titled data
-    ## close the in file
-    inFile = open(fp, "r")
-    data = inFile.readlines()
-    inFile.close()
+    results = []  ## holds function result
+    inFile = open(fp, "r")  ## open file target
+    data = [line for line in inFile.readlines() if
+            line.strip()]  ## imported file titled data and stripped of blank lines
+    inFile.close()  ## close the in file
 
-    ## declaration of variables used in the process
-    waves = []  ## holds wavelength name values
-    finalWaves = []  ## holds shifted wavelength values
+    ## Declaration of variables used in the process
+    waves = []  ## Holds the wavelength name values
+    waveList = []  ## Holds the values of waves with duplicate values removed
+    finalWaves = []  ## Holds shifted wavelength name values
 
-    spectra1 = []  ## empty list to hold spectra values
-    spectra1List = []  ## empty list to hold all average values for spectra
-    spectra1Total = 0  ## total of the spectra value - needs to be declared as 0 for the sake of if statement
-    finalSpectra1 = []  ## holds normalised spectra1 values
+    spectra1 = []  ## Empty list to hold spectra values
+    spectra1List = []  ## Empty list to hold all average values for spectra
+    spectra1Total = 0  ## Total of the spectra value - needs to be declared as 0 for the sake of if statement
+    finalSpectra1 = []  ## Holds normalised spectra1 values
 
-    spectra2 = []  ## empty list to hold spectra values
-    spectra2List = []  ## empty list to hold all average values for spectra
-    spectra2Total = 0  ## total of the spectra value - needs to be declared as 0 for the sake of if statement
-    finalSpectra2 = []  ## holds normalised spectra values
+    spectra2 = []  ## Empty list to hold spectra values
+    spectra2List = []  ## Empty list to hold all average values for spectra
+    spectra2Total = 0  ## Total of the spectra value - needs to be declared as 0 for the sake of if statement
+    finalSpectra2 = []  ## Holds normalised spectra values
 
-    spectra3 = []  ## empty list to hold spectra values
-    spectra3List = []  ## empty list to hold all average values for spectra
-    spectra3Total = 0  ## total of the spectra value - needs to be declared as 0 for the sake of if statement
-    finalSpectra3 = []  ## holds normalised spectra values
+    spectra3 = []  ## Empty list to hold spectra values
+    spectra3List = []  ## Empty list to hold all average values for spectra
+    spectra3Total = 0  ## Total of the spectra value - needs to be declared as 0 for the sake of if statement
+    finalSpectra3 = []  ## Holds normalised spectra values
 
-    counter = 0  ## holds value used to determine the number of unique wavelength names
-    occurance = 0  ## holds value used to determine the number of frames in the file
-    frames = 0  ## holds value used to store number of frames from the file
+    counter = 0  ## Holds value used to determine the number of unique wavelength names
+    occurance = 0  ## Holds value used to determine the number of frames in the file
+    frames = 0  ## Holds value used to store number of frames from the file
 
-    ## prompt user to enter an offset used to shift the wavelength - warns of the need to be numeric
-    # shiftInput = input(
-    #     "Enter the offset used to shift wavelength. Please note: Needs to be a numeric value. Please enter your selection here: ")
-    ## if the user enters a valid number
-    ## use this entry as a float as the shift offset
-    # if shiftInput.isnumeric():
-    #     shiftInput = float(shiftInput)
-    # ## if the user doesnt enter a valid number
-    # ## have them re enter the value - warn another error will result in use of default offset of 1
-    # else:
-    #     shiftInput = input(
-    #         "You did not enter a numeric value, another incorrect entry will result in default offset of 1 being used. Please enter a numeric offset to shift wavelength: ")
-    #     ## if the user enters a valid number
-    #     ## use this entry as a float as the shift offset
-    #     if shiftInput.isnumeric():
-    #         shiftInput = float(shiftInput)
-    #     ## if the user doesnt enter a valid number
-    #     ## used the default offset of 1 as the shift offset
-    #     else:
-    #         shiftInput = float(1)
+    ## ** DEBUGGING TOOLS **
+    debug = 0
+    totalToBug = (len(data))
 
-        ## for each line of the imported file
-    ## add the first three characters in the line to waveList and remove duplicate values
-    for line in data:
-        waves.append(line[:7])  ## need to rever from hardcode
-        waveList = list(dict.fromkeys(waves))
+    ## For each line of the imported file that dont begin with F (to remove Frame rows)
+    ## Split line to identify wavelength (first value in row) add to a waveList and remove duplicate values
+    while counter < len(data):
+        for line in data:
+            if not line.startswith("F"):
+                thisLine = line.split()
+                waves.append(thisLine[0])
+                waveList = list(dict.fromkeys(waves))
+                ## ** DEBUG TOOLS **
+            ## print system progress to user in percent via terminal
+            debug = debug + 1
+            print("PROCESS 1 / 4: ", round((debug / totalToBug) * 100), "% ", debug, "/", totalToBug)
+            counter = counter + 1  ## increase count by 1 to move to next row
 
-    ## while count is less than the length of the waveList
-    ## for each line of the imported file
+    ## ** RESET DEBUG TOOLS **
+    debug = 0
+    totalToBug = (len(waveList))
+
+    ## For each line of the imported file split line into wavelength, spectra values
+    ## Compare the current row wave to the waveList wave to allocate spectra
+    ## Take found spectra values and categorize into seperate lists (Supports up to 3 spectra values)
+    counter = 0
     while counter < len(waveList):
         for line in data:
-            ## if first three characters of the line are equal to the referenced wave length name
-            ## add that lines spectra values to the relevant list
-            ## increase the occurances of matching lines
-            if line[:7] == waveList[counter]:
-                spectra1.append(int(line[8:11]))  ## need to rever from hardcode
-                ## if two spectra values
-                if len(line) > 14:
-                    spectra2.append(int(line[12:15]))  ## need to rever from hardcode
-                ## if three spectra values
-                if len(line) > 18:
-                    spectra3.append(int(line[16:19]))  ## need to rever from hardcode
-                occurance = occurance + 1
+            thisLine = line.split()
+            if thisLine[0] == waveList[counter]:
+                spectra1.append(int(thisLine[1]))
+                ## if 2 spectra values found
+                if len(thisLine) > 2:
+                    spectra2.append(int(thisLine[2]))
+                    ## if 3 spectra values found
+                if len(thisLine) > 3:
+                    spectra2.append(int(thisLine[3]))
+                occurance = occurance + 1  ## increase occurance count by 1
 
-                ## if there was a match found during the if loop
-        ## add together all the values that were found to get the total
-        ## find the average value of all instances found and round to 1 decimal point
-        ## add the average values to a list with the other average values found
+        ## If there was a match found during the if loop
+        ## Add together all the values that were found to get the total
+        ## Find the average value of all instances found and round to 1 decimal point
+        ## Add the average values to a list with the other average values found
         if occurance > 0:
             spectra1Total = int(sum(spectra1))
             spectra1Average = round(spectra1Total / occurance, 1)
             spectra1List.append(spectra1Average)
 
-            ## if there is a second spectra value found
+            ## If there is a second spectra value found
             if spectra2Total > 0:
                 spectra2Total = int(sum(spectra2))
                 spectra2Average = round(spectra2Total / occurance, 1)
                 spectra2List.append(spectra2Average)
 
-            ## if there is a third spectra value found
+            ## If there is a third spectra value found
             if spectra3Total > 0:
                 spectra3Total = int(sum(spectra3))
                 spectra3Average = round(spectra3Total / occurance, 1)
                 spectra3List.append(spectra3Average)
 
-        ## empty the lists used to hold found values so they can be used for the next value
+        ## Empty the lists used to hold found values so they can be used for the next value
         spectra1 = []
         spectra2 = []
         spectra3 = []
 
-        ## if frames havent been recorded yet take the occurance value and move to frames
+        ## If the frames haven't been recorded yet, take the occurance value and move to frames
         if frames == 0:
             frames = occurance
+        occurance = 0  ## Reverts occurance count to zero
 
-        occurance = 0  ## reverts occurance count to zero
-        counter = counter + 1  ## increase count to search for next wavelength
+        ## ** DEBUG TOOLS **
+        ## print system progress to user in percent via terminal
+        debug = debug + 1
+        print("PROCESS 2 / 4: ", round((debug / totalToBug) * 100), "% ", debug, "/", totalToBug)
 
-    ## reset count value to 0
-    ## while the counter is less than the length of the waveList
-    ## for each line in the waveList
+        counter = counter + 1  ## Increase counter by one to search for the next wavelength
+
+    ## ** RESET DEBUG TOOLS **
+    debug = 0
+    totalToBug = (len(waveList))
+
+    ## While the counter is less than the length of the waveList go through the rows in waveList
     counter = 0
     while counter < len(waveList):
         for line in waveList:
-            ## runs the wavlength through the shift equation using the shift offset provided by user upon load
-            ## append rounded, shifted values to a final wave list
-            ## increase count by 1 to end while loop at end of list
+            ## Runs the wavelength through the shift equation using the shift offset provided by user upon input
+            ## Append the rounded value, shifted values to a final wave list
             shiftedWave = ((1 / shiftInput) - (1 / float(line))) * 10 ** 7
             shiftedWave = round(shiftedWave, 2)
             finalWaves.append(shiftedWave)
-            counter = counter + 1
 
-    ## reset count value to 0
-    ## while the counter is less than the length of spectra1List - if empty is 0 and = counter
-    ## for each line in spectra1List
-    ## check the value provided is greater than 0
+            ## ** DEBUG TOOLS **
+            ## print system progress to user in percent via terminal
+            debug = debug + 1
+            print("PROCESS 3 / 4: ", round((debug / totalToBug) * 100), "% ", debug, "/", totalToBug)
+
+            counter = counter + 1  ## Increase counter by one to search for the next wavelength
+
+    ## ** RESET DEBUG TOOLS **
+    debug = 0
+    totalToBug = (len(waveList))
+
+    ## Reset the counter value to 0
+    ## While the counter is less than the length of spectra1List - if empty is 0 and = counter
+    ## For each line in spectra1List
+    ## Check the value provided is greater than 0
     counter = 0
     while counter < len(spectra1List):
         for line in spectra1List:
             if spectra1List[counter] > 0:
-                ## run the spectraList1 values through the normalisation formula
-                ## append the rounded and normalised spectra value to a finalSpectra1 list
-                ## increase the counter by 1 to move to the next value in spectra1List
-                normalisedSpectra = (1 / frames * (spectra1List[counter]))
-                normalisedSpectra = round(normalisedSpectra, 1)
+                ## Append the rounded and normalised spectra value to a finalSpectra1 list
+                normalisedSpectra = round(spectra1List[counter], 1)
                 finalSpectra1.append(normalisedSpectra)
-                counter = counter + 1
 
-    ## reset count value to 0
-    ## while the counter is less than the length of spectra2List - if empty is 0 and = counter
-    ## for each line in spectra2List
-    ## check the value provided is greater than 0
+                ## ** DEBUG TOOLS **
+                ## print system progress to user in percent via terminal
+                debug = debug + 1
+                print("PROCESS 4 / 4: ", round((debug / totalToBug) * 100), "% ", debug, "/", totalToBug)
+
+                counter = counter + 1  ## Increase the counter by 1 to move to the next spectra value
+
+    ## ** RESET DEBUG TOOLS **
+    debug = 0
+    totalToBug = (len(waveList))
+
+    ## While the counter is less than the length of spectra2List - if empty is 0 and = counter
+    ## For each line in spectra2List
+    ## Check the value provided is greater than 0
     counter = 0
     while counter < len(spectra2List):
         for line in spectra2List:
-            ## run the spectraList1 values through the normalisation formula
-            ## append the normalised spectra value to a finalSpectra1 value list
-            ## increase the counter by 1 to move to the next value in spectra1List
-            normalisedSpectra = (1 / frames * (spectra2List[counter]))
-            finalSpectra2.append(normalisedSpectra)
-            counter = counter + 1
+            if spectra2List[counter] > 0:
+                ## Append the normalised spectra value to a finalSpectra1 value list
+                normalisedSpectra = round(spectra2List[counter], 1)
+                finalSpectra2.append(normalisedSpectra)
 
-    ## reset count value to 0
-    ## while the counter is less than the length of spectra3List - if empty is 0 and = counter
-    ## for each line in spectra3List
-    ## check the value provided is greater than 0
+                ## ** DEBUG TOOLS **
+                ## print system progress to user in percent via terminal
+                debug = debug + 1
+                print("PROCESSING EXTRA SPECTRA: ", round((debug / totalToBug) * 100, 1), "% ", debug, "/", totalToBug)
+
+                counter = counter + 1  ## Increase the counter by 1 to move to the next spectra value
+
+    ## ** RESET DEBUG TOOLS **
+    debug = 0
+    totalToBug = (len(waveList))
+
+    ## While the counter is less than the length of spectra3List - if empty is 0 and = counter
+    ## For each line in spectra3List
+    ## Check the value provided is greater than 0
     counter = 0
     while counter < len(spectra3List):
         for line in spectra3List:
-            ## run the spectraList3 values through the normalisation formula
-            ## append the normalised spectra value to a finalSpectra3 value list
-            ## increase the counter by 1 to move to the next value in spectra3List
-            normalisedSpectra = (1 / frames * (spectra3List[counter]))
-            finalSpectra3.append(normalisedSpectra)
-            counter = counter + 1
+            if spectra3List[counter] > 0:
+                ## Append the normalised spectra value to a finalSpectra3 value list
+                normalisedSpectra = round(spectra3List[counter], 1)
+                finalSpectra3.append(normalisedSpectra)
+
+                ## ** DEBUG TOOLS **
+                ## print system progress to user in percent via terminal
+                debug = debug + 1
+                print("PROCESSING EXTRA SPECTRA: ", round((debug / totalToBug) * 100, 1), "% ", debug, "/", totalToBug)
+
+                counter = counter + 1  ## Increase the counter by 1 to move to the next spectra value
+
+    ## ** DEBUG TOOLS **
+    ## print system status to user via terminal
+    print("MAPPING DATA")
 
     # ------------------------------------------------- #
     if finalSpectra1 != [] and finalSpectra2 == []:
@@ -221,6 +253,69 @@ def read_normalised_data(fp):
             _ = [float(i) for i in _]
             data.append(_)
     return data
+
+
+def display_specs():
+    """function to pull labbook details for current raman file"""
+
+    ## NEED TO PASS FROM FP - NEEDS TO BE FOR THE FILE WHICH IS DISPLAYED CURRENTLY ON TYHE NORMALISED TAB
+    dataFile = open("C:/Users/End User/iCloudDrive/Desktop/ICTCode/data/raman_12.txt", "r")
+    thisFile = dataFile.name
+
+    ## NEED TO HAVE LIUYU CREATE NEW IMPORT SECTION FOR THIS FILE IMPORT - LABBOOK WITH READING SPECS
+    inFile = open("C:/Users/End User/iCloudDrive/Desktop/ICTCode/data/labbook-2018-10-30.txt", "r")
+    labbook = [line for line in inFile.readlines() if line.strip()]
+    inFile.close()
+
+    ## declare variables for function to allow for loop entry
+    fileSpecs = []
+    name = 'null'
+    counter=0
+
+    ## while counter less than the number of lines in the labbook go through the lines in labbook
+    ## if the line is deemed a reading entry, split the data at the spaces, strip the punctuation.
+    ## check the first value from the split and check if it matched the inFile name, if a match allocate data to individual variables
+    while counter < len(labbook):
+        for line in labbook:
+            if len(line) > 25:
+                thisLine = line.split()
+                name = thisLine[0]
+                name = name[:-1]
+                if name in thisFile:
+                    fileSpecName = 'File: ' + name
+                    time = thisLine[1]
+                    fileSpecTime = 'Time: ' + time[:-1]
+                    P = thisLine[2]
+                    fileSpecP = 'P: ' + P[2:-1]
+                    E = thisLine[5] + thisLine[6]
+                    fileSpecE = 'E: ' + E[:-1]
+                    g = thisLine[7]
+                    fileSpecG = 'g: ' + g[:-1]
+                    c = thisLine[8]
+                    fileSpecC = 'c: ' + c[2:-1]
+                    temp = thisLine[9]
+                    fileSpecTemp = 'Temp: ' + temp
+            counter = counter + 1 ## increase count by 1 to move to next line
+
+    ## check if a match was found between the inFile and the labbook
+    ## if yes, update the name to matching format and assign the data from the matched row as the return data
+    if name != 'null':
+        fileSpecName = fileSpecName
+        fileSpecs.append(fileSpecName)
+        fileSpecs.append(fileSpecTime)
+        fileSpecs.append(fileSpecP)
+        fileSpecs.append(fileSpecE)
+        fileSpecs.append(fileSpecG)
+        fileSpecs.append(fileSpecC)
+        fileSpecs.append(fileSpecTemp)
+    ## if no, provide message to inform user
+    else:
+        fileSpecName = ' No File Specs Found'
+        fileSpecs.append(fileSpecName)
+
+    ## close the data file and return the results
+    dataFile.close()
+    return fileSpecs
 
 if __name__ == '__main__':
     fp = 'data/raman_01.txt'
